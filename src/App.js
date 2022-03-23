0245451ac1f9adbafe741a8958033df4745216e1
Mariana Hodniki Davie <marianahodniki1991@gmail.com>
2022-03-22 22:11:56 -0600

    pokegame starter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import Pokecard from './Pokecard/Pokecard.js';
+// import Pokecard from './Pokecard.js';
+import Pokegame from './Pokegame.js';
 // import './App.css';
 
 class App extends Component {
   render(){
     return(
       <div className="App">
-        <Pokecard/>
+        {/* <Pokecard/> */}
+        <Pokegame/>
       </div>
     );
   }
