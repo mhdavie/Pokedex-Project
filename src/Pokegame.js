91adb98d8632670b4c59450c38cc05996564a72f
Mariana Hodniki Davie <marianahodniki1991@gmail.com>
2022-03-25 21:55:02 -0600

    pokegame added

--- src/Pokegame.js
+++ src/Pokegame.js
@@ -1,9 +1,8 @@
-
 import React, { Component } from 'react';
-
+import Pokedex from './Pokedex';
 
 class Pokegame extends Component {
-    static defaultProps = {
+	static defaultProps = {
 		pokemon : [
 			{ id: 4, name: 'Charmander', type: 'fire', base_experience: 62 },
 			{ id: 7, name: 'Squirtle', type: 'water', base_experience: 63 },
@@ -15,13 +14,22 @@ class Pokegame extends Component {
 			{ id: 133, name: 'Eevee', type: 'normal', base_experience: 65 }
 		]
 	};
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+	render() {
+		let hand1 = [];
+		let hand2 = [ ...this.props.pokemon ];
+		while (hand1.length < hand2.length) {
+			let randIdx = Math.floor(Math.random() * hand2.length);
+			let randPokemon = hand2.splice(randIdx, 1)[0];
+			hand1.push(randPokemon);
+		}
+		let exp1 = hand1.reduce((exp, pokemon) => exp + pokemon.base_experience, 0);
+		let exp2 = hand2.reduce((exp, pokemon) => exp + pokemon.base_experience, 0);
+		return (
+			<div>
+				<Pokedex pokemon={hand1} exp={exp1} isWinner={exp1 > exp2} />
+				<Pokedex pokemon={hand2} exp={exp2} isWinner={exp2 > exp1} />
+			</div>
+		);
+	}
 }
-
 export default Pokegame;
